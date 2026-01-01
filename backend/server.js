3ed1cbf25cd57a414ba59c58a79cb8b518ede8ce
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-01-01 19:31:30 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(() => console.log("MongoDB error"));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("SmartHire backend running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
